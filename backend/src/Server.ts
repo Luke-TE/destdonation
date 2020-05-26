c66ef988e93f3c34ca6abcf9d9c7d2bb8c25e429
Luke Texon <luk.tex37@gmail.com>
2020-05-26 16:24:14 +0100

    [ljt18] Added Cors

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import path from 'path';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import { BAD_REQUEST } from 'http-status-codes';
@@ -23,6 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(cors());
 
 // Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
