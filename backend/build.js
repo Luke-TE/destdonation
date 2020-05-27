d38bd408ff167f4a6c27a6a2cda1e1f47be3c2d9
Luke Texon <luk.tex37@gmail.com>
2020-05-27 23:33:06 +0100

    [ljt18] Converted express server to serve the react files

--- backend/build.js
+++ backend/build.js
@@ -5,8 +5,7 @@ try {
   // Remove current build
   fs.removeSync("./dist/");
   // Copy front-end files
-  fs.copySync("./src/public", "./dist/public");
-  fs.copySync("./src/views", "./dist/views");
+  fs.copySync("../frontend/build", "./dist/public");
   // Transpile the typescript files
   childProcess.exec("tsc --build tsconfig.prod.json");
 } catch (err) {
