bf4f32e54d7b22cdf14f8d6ce57405a2cbcc2943
Luke Texon <luk.tex37@gmail.com>
2020-05-29 02:11:23 +0100

    [ljt18] Added DailyUpdate to current donation page

--- client/src/components/CurrentDonation/CurrentDonation.tsx
+++ client/src/components/CurrentDonation/CurrentDonation.tsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import Timeline from "./Timeline/Timeline";
 import "./CurrentDonation.css";
+import DailyUpdate from "../DailyUpdate/DailyUpdate";
 
 class CurrentDonation extends Component {
   render() {
     // @ts-ignore
     return (
       <div className="page-content">
-        <h1>CurrentDonation</h1>
-        <p>Current Donation body content</p>
+        <h1>Current Donation</h1>
+        <DailyUpdate foodWeight={100} numOfMeals={200}></DailyUpdate>
         <Timeline />
       </div>
     );
